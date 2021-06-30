347b904a00a8e5a8c19d507719f87dbc3cf63aa2
Jorge Olaizola <jorgelolaizola@hotmail.com>
2021-06-30 20:20:07 -0300

    DB connected

---
+++ server/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+const { USER_HOST, USER_DB } = process.env
+
+const MONGODB_URI = `mongodb://${USER_HOST}/${USER_DB}`
+
+mongoose.connect(MONGODB_URI, {
+    useUnifiedTopology: true,
+    userNewUrlParse: true
+})
+    .then(db => console.log('Database is connected'))
+    .catch(err => console.log(err));
--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const next = require('next');
+require('./db')
+require('dotenv').config()
 
 const port = process.env.PORT || 3000;
 const dev  = process.env.NODE_ENV !== 'production';
